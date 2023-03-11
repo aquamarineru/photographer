f9d373ba6778faa86cd4a8d895295c64af0d5cd5
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-03-11 23:56:48 +0100

    home bg

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,10 +6,8 @@ export default function Home() {
   return (
     <section className="home">
         <div className="home__container container grid">
-            <h1 className="home__title">Yana Korobeinik</h1>
-            <div className="home__description">
-                <p>I am a freelance phographger based in Athens</p>
-            </div>
+           {/*  <h1 className="home__title">Yana Korobeinik</h1> */}
+            <div className="home__black"></div>
         </div>
     </section>
   )
